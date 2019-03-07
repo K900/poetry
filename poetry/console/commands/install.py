85caa228967b12ed46c01030babfd5034c346dab
Mohamed Seleem <seleem1337@users.noreply.github.com>
2019-03-07 13:54:52 +0100

    Install: option to skip current project package (#757)

--- poetry/console/commands/install.py
+++ poetry/console/commands/install.py
@@ -9,6 +9,7 @@ class InstallCommand(EnvCommand):
 
     install
         { --no-dev : Do not install dev dependencies. }
+        { --no-root : Do not install the root package (your project). }
         { --dry-run : Outputs the operations but will not execute anything
                       (implicitly enables --verbose). }
         { --E|extras=* : Extra sets of dependencies to install. }
@@ -56,6 +57,9 @@ exist it will look for <comment>pyproject.toml</> and do the same.
 
         if return_code != 0:
             return return_code
+
+        if not self.option("no-root"):
+            return 0
 
         try:
             builder = EditableBuilder(self.poetry, self._env, NullIO())
